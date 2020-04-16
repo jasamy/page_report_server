bb7c6692caeb0efc034270001c20746ae9c1e43a
Jason Byers <jason.byers@mindtools.com>
2020-04-16 10:16:58 +0100

    added cors support

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const cors = require('cors');
 const config = require('config')
 const dbConn = require('./lib/db')
 const app = express()
+
+app.use(cors());
 
 app.get("/data", (req, res, next) => {
     res.set("content-type", "application/json")
